525938450de9c079c7fcc80ffc13ce1d770c7cd4
Josh Johnson <snofox@snofox.net>
2021-08-07 23:02:41 -0700

    Allow vine to grow with bonemeal

--- src/main/java/net/snofox/minecraft/weedwacker/WeedWacker.java
+++ src/main/java/net/snofox/minecraft/weedwacker/WeedWacker.java
@@ -1,13 +1,23 @@
 package net.snofox.minecraft.weedwacker;
 
+import org.bukkit.Effect;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockSpreadEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class WeedWacker extends JavaPlugin implements Listener {
 
@@ -26,6 +36,27 @@ public final class WeedWacker extends JavaPlugin implements Listener {
             case TWISTING_VINES:
                 ev.setCancelled(!isBoneBase(ev.getNewState()));
         }
+    }
+
+    @EventHandler
+    public void onPlayerInteract(final PlayerInteractEvent ev) {
+        if(!(ev.getAction().equals(Action.RIGHT_CLICK_BLOCK) && Objects.requireNonNull(ev.getHand()).equals(EquipmentSlot.HAND))) return;
+        final ItemStack handItem = ev.getItem();
+        if(handItem == null) return;
+        if(!handItem.getType().equals(Material.BONE_MEAL)) return;
+        final Block clickedBlock = ev.getClickedBlock();
+        if(!Objects.requireNonNull(clickedBlock).getType().equals(Material.VINE)) return;
+        if(growVine(clickedBlock)) {
+            updateInventory(ev.getPlayer(), handItem);
+            clickedBlock.getWorld().playEffect(clickedBlock.getLocation(), Effect.VILLAGER_PLANT_GROW, 15);
+        }
+    }
+
+    private void updateInventory(final Player player, final ItemStack hand) {
+        if(player.getGameMode().equals(GameMode.CREATIVE)) return;
+        if(hand.getAmount() == 1) hand.setType(Material.AIR);
+        else hand.setAmount(hand.getAmount()-1);
+        player.getInventory().setItemInMainHand(hand);
     }
 
     private boolean isBoneBase(final BlockState block) {
@@ -50,4 +81,19 @@ public final class WeedWacker extends JavaPlugin implements Listener {
             return foundType.equals(Material.BONE_BLOCK);
         }
     }
+
+    private boolean growVine(final Block block) {
+        for(int searchY = 1; searchY <= block.getY() - block.getWorld().getMinHeight(); ++searchY) {
+            final Block searchBlock = block.getRelative(0, -searchY, 0);
+            if(searchBlock.getType().equals(Material.VINE)) continue;
+            if(searchBlock.isEmpty()) {
+                searchBlock.setType(Material.VINE);
+                searchBlock.setBlockData(searchBlock.getRelative(BlockFace.UP).getBlockData());
+                return true;
+            }
+            break;
+        }
+        return false;
+    }
+
 }
